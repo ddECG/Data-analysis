48f2be28b0f8459625ee294e3ebc510f3928aa32
andersaskeland <anders.askeland@gmail.com>
2022-03-29 20:35:34 +0200

    Initial clean

---
+++ data-raw/import_data.py
@@ -0,0 +1,15 @@
+# Imports
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import wfdb
+import ast
+
+# Open data
+data=pd.read_csv("/data-raw/ptb-xl/ptbxl_database.csv")
+
+# Remove records w. pacemaker
+
+
+# 
